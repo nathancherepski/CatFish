dca4156b9f7b29363b487eb20c162ae9150c1a4c
nathancherepski <80000091+nathancherepski@users.noreply.github.com>
2022-05-16 16:04:30 -0500

    Updated Main
    Added individual pages and navigation

--- WorkingNav/App.js
+++ WorkingNav/App.js
@@ -1,20 +1,155 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+/*
+const DBAbstraction = require('./DBAbstraction');
 
-export default function App() {
+const path = require('path');
+
+const pathToData = path.join(__dirname, 'dbs', 'loginData.sqlite');
+const db = new DBAbstraction(pathToData);
+*/
+// helpers
+
+function checkCredentials(username, password){
+  /*
+  try {
+    const results = db.checkCreds(username, password);
+    
+    if(results != err && results != null)
+    {
+      return true;
+    }
+  }catch(err)
+  {
+    return false;
+  }
+  */
+  return true;
+
+   // database code here
+   
+}
+
+
+
+
+
+
+
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Catfish</Text>
+        <Button
+          title="Login"
+          onPress={() => this.props.navigation.navigate('Login')}
+        />
+      </View>
+    );
+  }
+}
+
+class MatchesScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Matches Screen </Text>
+        <Button
+          title="Go to Potentials"
+          onPress={() => this.props.navigation.navigate('Potentials')}
+        />
+      </View>
+    );
+  }
+}
+
+class PotentialsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Potentials</Text>
+        <Button
+          title="Go to Potentials"
+          onPress={() => this.props.navigation.navigate('Login')}
+        />
+      </View>
+    );
+  }
+}
+
+
+class LoginScreen extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state={
+        email:'',
+        password:''
+    }
+    this.trylogin = this.trylogin.bind(this); // you need this to be able to access state from login
+  }
+
+  trylogin() {
+    if(checkCredentials(this.state.username, this.state.password)){
+      console.log('your username is', this.state.username);
+      console.log('your password is', this.state.password);
+      return true;
+    }
+    else
+    {
+      console.log('invalid login');
+      return false;
+    }
+    
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Enter your username and pasword below.</Text>
+        <TextInput style={{height: 40}} placeholder="username" defaultValue={''} id="username" onChangeText={ (username) => {
+    this.setState({username})}}></TextInput>
+        <TextInput secureTextEntry={true} style={{height: 40}} placeholder="password" defaultValue={''} id="password" onChangeText={(password) => {
+    this.setState({password})}}></TextInput>
+        <Button
+          title="Login"
+          onPress={() => {
+            if(this.trylogin() == true){
+              this.props.navigation.push('Matches')
+              
+            }
+          }
+        }
+        />
+      </View>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Matches" component={MatchesScreen} />
+        <Stack.Screen name="Potentials" component={PotentialsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
