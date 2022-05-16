b889fec720a72c4192cc82fbd0d34efd19dfed1d
Numba-Seven <100711106+Numba-Seven@users.noreply.github.com>
2022-05-16 10:57:44 +0500

    Add files via upload

---
+++ thirdTry/CatFish2/assets/components/MaterialButtonDanger.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+function MaterialButtonDanger(props) {
+  return (
+    <TouchableOpacity style={[styles.container, props.style]}
+      onPress = {()=>props.navigation.navigate('Details')}
+    >
+      <Text style={styles.caption}>{props.caption || "BUTTON"}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F44336",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    borderRadius: 2,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.35,
+    shadowRadius: 5,
+    elevation: 2,
+    minWidth: 88,
+    paddingLeft: 16,
+    paddingRight: 16
+  },
+  caption: {
+    color: "#fff",
+    fontSize: 23
+  }
+});
+
+export default MaterialButtonDanger;
