108b1184b73917345ac22303137086dff47dd8c4
nathancherepski <80000091+nathancherepski@users.noreply.github.com>
2022-05-16 23:32:29 -0500

    Couple more fixes

--- WorkingNav/App.js
+++ WorkingNav/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './pages/HomeScreen';
-import NewLoginScreen from './pages/NewLoginScreen';
+import LoginScreen from './pages/LoginScreen';
 import MatchesScreen from './pages/MatchesScreen';
 import Messages from './pages/Messages';
 
@@ -18,7 +18,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={NewLoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Matches" component={MatchesScreen} />
         <Stack.Screen name="Messages" component={Messages} />
       </Stack.Navigator>
